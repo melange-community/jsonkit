melange-community/melange-json

feat: change the naming to jsonkit

flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "melange-json Nix Flake";
+  description = "jsonkit Nix Flake";
 
   inputs.nixpkgs.url = "github:nix-ocaml/nix-overlays";
 
@@ -16,8 +16,8 @@
     {
       packages = forAllSystems (pkgs:
         let
-          melange-json = with pkgs.ocamlPackages; buildDunePackage {
-            pname = "melange-json";
+          jsonkit = with pkgs.ocamlPackages; buildDunePackage {
+            pname = "jsonkit";
             version = "dev";
 
             src =
@@ -27,8 +27,8 @@
                 fileset = fs.unions [
                   ./dune-project
                   ./dune
-                  ./melange-json.opam
-                  ./melange-json-native.opam
+                  ./jsonkit.opam
+                  ./jsonkit-native.opam
                   ./src
                   ./ppx
                 ];
@@ -42,12 +42,12 @@
             ];
           };
         in
-        { inherit melange-json; default = melange-json; });
+        { inherit jsonkit; default = jsonkit; });
       devShells = forAllSystems (pkgs:
         let
           inherit (pkgs) nodejs_latest ocamlPackages system yarn;
           mkShell = { buildInputs ? [ ] }: pkgs.mkShell {
-            inputsFrom = [ self.packages.${system}.melange-json ];
+            inputsFrom = [ self.packages.${system}.jsonkit ];
             nativeBuildInputs = [
               yarn
               nodejs_latest

ppx/jsonschema/ppx_deriving_jsonschema.ml
@@ -623,9 +623,9 @@ let sig_type_decl ~ctxt ast _flag_polymorphic_variant_tuple
       in
       [ psig_extension ~loc ext [] ]
 
-(* Registration is performed explicitly by the melange-json ppx entry points
+(* Registration is performed explicitly by the jsonkit ppx entry points
    (ppx_deriving_json_native.ml / ppx_deriving_json_js.ml) rather than at module
-   load time, so the jsonschema deriver ships as part of melange-json[-native].ppx. *)
+   load time, so the jsonschema deriver ships as part of jsonkit[-native].ppx. *)
 let register () =
   Deriving.add deriver_name
     ~str_type_decl:

melange-community/jsonkit

flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "melange-json Nix Flake";
+  description = "jsonkit Nix Flake";
 
   inputs.nixpkgs.url = "github:nix-ocaml/nix-overlays";
 
@@ -16,8 +16,8 @@
     {
       packages = forAllSystems (pkgs:
         let
-          melange-json = with pkgs.ocamlPackages; buildDunePackage {
-            pname = "melange-json";
+          jsonkit = with pkgs.ocamlPackages; buildDunePackage {
+            pname = "jsonkit";
             version = "dev";
 
             src =
@@ -27,8 +27,8 @@
                 fileset = fs.unions [
                   ./dune-project
                   ./dune
-                  ./melange-json.opam
-                  ./melange-json-native.opam
+                  ./jsonkit-melange.opam
+                  ./jsonkit.opam
                   ./src
                   ./ppx
                 ];
@@ -42,12 +42,12 @@
             ];
           };
         in
-        { inherit melange-json; default = melange-json; });
+        { inherit jsonkit; default = jsonkit; });
       devShells = forAllSystems (pkgs:
         let
           inherit (pkgs) nodejs_latest ocamlPackages system yarn;
           mkShell = { buildInputs ? [ ] }: pkgs.mkShell {
-            inputsFrom = [ self.packages.${system}.melange-json ];
+            inputsFrom = [ self.packages.${system}.jsonkit ];
             nativeBuildInputs = [
               yarn
               nodejs_latest

ppx/jsonschema/ppx_deriving_jsonschema.ml
@@ -623,9 +623,9 @@ let sig_type_decl ~ctxt ast _flag_polymorphic_variant_tuple
       in
       [ psig_extension ~loc ext [] ]
 
-(* Registration is performed explicitly by the melange-json ppx entry points
+(* Registration is performed explicitly by the jsonkit ppx entry points
    (ppx_deriving_json_native.ml / ppx_deriving_json_js.ml) rather than at module
-   load time, so the jsonschema deriver ships as part of melange-json[-native].ppx. *)
+   load time, so the jsonschema deriver ships as part of jsonkit[-melange].ppx. *)
 let register () =
   Deriving.add deriver_name
     ~str_type_decl: