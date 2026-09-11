c026b543cea6fa03cd80179b5f9073c2896e6b2a
Pedro Lisboa <pedrobslisboa@gmail.com>
2026-09-11 17:35:09 +0000

    test: compare the e2e runtime output with dune diff rules

---
+++ ppx/test/e2e/shared/run.ml
@@ -0,0 +1,4 @@
+let () = print_endline "*** json deriver tests ***"
+let () = Cases.test ()
+let () = print_endline "*** json_string deriver tests ***"
+let () = Json_string_cases.test ()
--- ppx/test/gen_test.sh
+++ ppx/test/gen_test.sh
@@ -1,6 +1,6 @@
 echo "  $ alias run='$1 -impl - | ocamlformat - --impl'";
 echo "";
-cat ./example.ml | grep '^type' | while read line; do
+cat ./e2e/shared/cases.ml | grep '^type' | while read line; do
   echo '  $ cat <<"EOF" | run';
   echo "  > $line";
   echo '  > EOF';
